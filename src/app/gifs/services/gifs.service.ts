2cf3b88cb54c2c6ca4e84af4eaeed21f609c27c4
Gerardo <gera_lps@hotmail.com.ar>
2021-08-08 04:32:31 -0300

    first commit

---
+++ src/app/gifs/services/gifs.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GifsService {
+  private apiKey: string = 'PJbCRwhx8i0GhTRbiqmKOoLLibEeBZPQ';
+  private _historial: string[] = [];
+
+  public resultados: Gif[] = [];
+
+  get historial() {
+    return [...this._historial];
+  }
+
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+  }
+
+  buscarGifs(query: string = '') {
+    query = query.trim().toLowerCase();
+
+    if (!this._historial.includes(query)) {
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
+
+    const resp = this.http
+      .get<SearchGifsResponse>(
+        `https://api.giphy.com/v1/gifs/search?api_key=PJbCRwhx8i0GhTRbiqmKOoLLibEeBZPQ&q=${query}&limit=10`
+      )
+      .subscribe((resp) => {
+        console.log(resp.data);
+        this.resultados = resp.data;
+      });
+  }
+}
